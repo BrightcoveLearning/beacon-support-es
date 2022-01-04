2758bcfefca74c460f0120b31db54b8a1d80828d
Robert Crooks <rcrooks@brightcove.com>
2022-01-04 16:05:00 -0500

    new files

--- assets/js/feedback-beacon.js
+++ assets/js/feedback-beacon.js
@@ -1,5 +1,9 @@
+---
+layout: null
+---
+
 var fby = fby || [];
-fby.push(['showTab', {id: '14941', position: 'right', color: 'rgb(28, 185, 213)'}]);
+fby.push(['showTab', {id: '14941', position: 'right', color: '{{ site.product_color }}'}]);
 (function () {
     var f = document.createElement('script'); f.type = 'text/javascript'; f.async = true;
     f.src = '//cdn.feedbackify.com/f.js';
